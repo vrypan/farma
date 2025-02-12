db0131fab15652b8ee45723e44991d6eb82e974b
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2025-02-12 18:28:34 +0200

    setup script improvements

--- cmd/setup.go
+++ cmd/setup.go
@@ -24,6 +24,7 @@ func init() {
 
 func setupFidr(cmd *cobra.Command, args []string) {
 
+	fmt.Println()
 	configDir, err := config.ConfigDir()
 	if err != nil {
 		fmt.Printf("%v\n", err)
@@ -31,6 +32,30 @@ func setupFidr(cmd *cobra.Command, args []string) {
 	}
 	fmt.Printf("Config file is %s/%s\n", configDir, "farma.yaml")
 	fmt.Println(" > Make sure you edit it to set your hub.")
+	fmt.Println()
+
+	fmt.Println("Generating keys")
+	pubKeyHex := config.GetString("key.public")
+	privKeyHex := config.GetString("key.private")
+	if pubKeyHex != "" || privKeyHex != "" {
+		fmt.Println(" > A key already exists. Not generarting a new one.")
+		fmt.Printf(" > Private key: %s\n", privKeyHex)
+		fmt.Printf(" > Public key: %s\n", pubKeyHex)
+	} else {
+		pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
+		if err != nil {
+			fmt.Printf(" > Error generating keypair: %v\n", err)
+			os.Exit(1)
+		}
+		pubKeyHex = fmt.Sprintf("0x%x", pubKey)
+		privKeyHex = fmt.Sprintf("0x%x", privKey)
+		fmt.Printf(" > Private key: %s\n", privKeyHex)
+		fmt.Printf(" > Public key: %s\n", pubKeyHex)
+		viper.Set("key.public", pubKeyHex)
+		viper.Set("key.private", privKeyHex)
+		viper.WriteConfig()
+	}
+	fmt.Printf(" > View/Edit your keypair in %s/%s\n", configDir, "farma.yaml")
 	fmt.Println()
 
 	dbPath := db.GetDbPath()
@@ -50,25 +75,4 @@ func setupFidr(cmd *cobra.Command, args []string) {
 	}
 	fmt.Println()
 
-	fmt.Println("Generating keys")
-	pubKeyHex := config.GetString("key.public")
-	privKeyHex := config.GetString("key.private")
-	if pubKeyHex != "" || privKeyHex != "" {
-		fmt.Println(" > The config file already has a key. Not generarting new one.")
-		fmt.Printf(" > Private key: %s\n", privKeyHex)
-		fmt.Printf(" > Public key: %s\n", pubKeyHex)
-	} else {
-		pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
-		if err != nil {
-			fmt.Printf(" > Error generating keypair: %v\n", err)
-			os.Exit(1)
-		}
-		pubKeyHex = fmt.Sprintf("0x%x", pubKey)
-		privKeyHex = fmt.Sprintf("0x%x", privKey)
-		fmt.Printf(" > Private key: %s\n", privKeyHex)
-		fmt.Printf(" > Public key: %s\n", pubKeyHex)
-		viper.Set("key.public", pubKeyHex)
-		viper.Set("key.private", privKeyHex)
-		viper.WriteConfig()
-	}
 }
