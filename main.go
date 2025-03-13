0db74d09a1a0ee6c8ef9020bb4937351073e27db
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2025-03-13 08:17:35 +0200

    Removed embed_fs

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"embed"
-
 	"github.com/vrypan/farma/cmd"
 )
 
-//go:embed test_frame/* test_frame/.well-known
-var staticFiles embed.FS
-
 func main() {
-	cmd.StaticFiles = staticFiles
 	cmd.Execute()
 }
