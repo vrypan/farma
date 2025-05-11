6aa4454e06166d363100c929ace7b71d8ecae32f
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2025-05-11 09:36:41 +0300

    Key, Json, Type methods. Removed unused code.

--- models/notification.go
+++ models/notification.go
@@ -1,14 +1,11 @@
 package models
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
 	"github.com/google/uuid"
 	db "github.com/vrypan/farma/localdb"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -38,116 +35,18 @@ func NewNotification(
 	}
 }
 
-func (n *Notification) Send_original() error {
-	_, err := n.Save()
+func (n *Notification) Key() string {
+	return n.Prefix() + fmt.Sprintf("%03d", *n.Version)
+}
+func (n *Notification) Json() []byte {
+	json, err := protojson.Marshal(n)
 	if err != nil {
-		return fmt.Errorf("Error saving notification: %w", err)
+		return nil
 	}
-
-	var tokenKeys []string
-	for k := range n.Tokens {
-		tokenKeys = append(tokenKeys, k)
-	}
-	data := map[string]any{
-		"notificationId": n.Id,
-		"title":          n.Title,
-		"body":           n.Message,
-		"targetUrl":      n.Link,
-		"tokens":         tokenKeys,
-	}
-
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return fmt.Errorf("Error marshalling json: %w", err)
-	}
-	request, err := http.NewRequest("POST", n.Endpoint, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return fmt.Errorf("Error creating new request: %w", err)
-	}
-
-	request.Header.Set("Content-Type", "application/json")
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return fmt.Errorf("Error making request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("Failed to send notification, status code: %d, URL: %s, tokens: %v",
-			response.StatusCode, n.Endpoint, n.Tokens)
-	}
-
-	bodyBytes, err := io.ReadAll(response.Body)
-	if err != nil {
-		return fmt.Errorf("Error reading response body: %w", err)
-	}
-
-	// Parse response body and record the status of each token
-	var responseBody struct {
-		Result struct {
-			SuccessfulTokens  []string `json:"successfulTokens"`
-			InvalidTokens     []string `json:"invalidTokens"`
-			RateLimitedTokens []string `json:"rateLimitedTokens"`
-		} `json:"result"`
-	}
-	err = json.Unmarshal(bodyBytes, &responseBody)
-	if err != nil {
-		return fmt.Errorf("Error unmarshalling response body: %w", err)
-	}
-	n.SuccessTokens = responseBody.Result.SuccessfulTokens
-	n.FailedTokens = responseBody.Result.InvalidTokens
-	n.RateLimitedTokens = responseBody.Result.RateLimitedTokens
-	context := EventContextNotification{Id: n.Id, Version: *n.Version}
-	for _, token := range n.SuccessTokens {
-		subscription := NewSubscription().FromKey(n.FrameId, n.Tokens[token], n.AppId)
-		l := UserLog{
-			FrameId:    subscription.FrameId,
-			UserId:     subscription.UserId,
-			AppId:      subscription.AppId,
-			EvtType:    EventType_NOTIFICATION_SENT,
-			EvtContext: &UserLog_EventContextNotification{EventContextNotification: &context},
-		}
-		err = l.Save()
-		if err != nil {
-			fmt.Printf("Error in UserLog.Save(): %v\n", err)
-		}
-	}
-	for _, token := range n.FailedTokens {
-		subscription := NewSubscription().FromKey(n.FrameId, n.Tokens[token], n.AppId)
-		l := UserLog{
-			FrameId:    subscription.FrameId,
-			UserId:     subscription.UserId,
-			AppId:      subscription.AppId,
-			EvtType:    EventType_NOTIFICATION_FAILED_INVALID,
-			EvtContext: &UserLog_EventContextNotification{EventContextNotification: &context},
-		}
-		err = l.Save()
-		if err != nil {
-			fmt.Printf("Error in UserLog.Save(): %v\n", err)
-		}
-		subscription.Status = SubscriptionStatus_UNSUBSCRIBED
-		subscription.Token = ""
-		subscription.Save()
-	}
-	for _, token := range n.RateLimitedTokens {
-		subscription := NewSubscription().FromKey(n.FrameId, n.Tokens[token], n.AppId)
-		l := UserLog{
-			FrameId:    subscription.FrameId,
-			UserId:     subscription.UserId,
-			AppId:      subscription.AppId,
-			EvtType:    EventType_NOTIFICATION_FAILED_RATE_LIMIT,
-			EvtContext: &UserLog_EventContextNotification{EventContextNotification: &context},
-		}
-		err = l.Save()
-		if err != nil {
-			return fmt.Errorf("Error in UserLog.Save(): %v\n", err)
-		}
-		subscription.Status = SubscriptionStatus_RATE_LIMITED
-		subscription.Token = token
-		subscription.Save()
-	}
-	return nil
+	return json
+}
+func (n *Notification) Type() string {
+	return "Notification"
 }
 
 func (n *Notification) Prefix() string {
